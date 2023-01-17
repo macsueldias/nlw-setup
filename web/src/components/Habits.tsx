01d11fc39937f5e7881934bcc8953d6971ef1199
Macsuel Dias <macsueldias@hotmail.com>
2023-01-17 15:58:36 -0300

    first class

--- web/src/components/Habits.tsx
+++ web/src/components/Habits.tsx
@@ -4,6 +4,19 @@ interface HabitsProps {
 
 export function Habits(props: HabitsProps) {
     return (
-        <h1 className="bg-slate-600 w-10 h-7 text-white m-2 rounded flex items-center justify-center">{props.completed}</h1>
+        <h1 
+            className="
+                bg-slate-600 
+                w-10 
+                h-7 
+                text-white 
+                m-2 rounded 
+                flex 
+                items-center 
+                justify-center
+            "
+        >
+            {props.completed}
+        </h1>
     )
 }
