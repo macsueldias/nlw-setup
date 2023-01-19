c9a09e6428b0c5dbfb111c75028de90e9dfb1fdb
Macsuel Dias <macsueldias@hotmail.com>
2023-01-18 22:33:01 -0300

    secondary class

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -31,7 +31,10 @@ export function SummaryTable() {
                 })}
                 {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
                     return (
-                        <div key={i} className="w-10 h-10 bg-zinc-900 border-zinc-800 border-2 rounded-lg opacity-40 cursor-not-allowed"></div>
+                        <div 
+                            key={i} 
+                            className="w-10 h-10 bg-zinc-900 border-zinc-800 border-2 rounded-lg opacity-40 cursor-not-allowed"
+                        />
                     )
                 })}
             </div>
