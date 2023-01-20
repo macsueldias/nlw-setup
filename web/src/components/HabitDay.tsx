a97edc874ab8eb4fe77301c895101314cff2bd39
Macsuel Dias <macsueldias@hotmail.com>
2023-01-19 22:56:37 -0300

    third class

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,9 +1,26 @@
+import * as Popover from '@radix-ui/react-popover'
+import { ProgressBar } from './ProgressBar'
+
 interface HabitDayProps {
     completed: number
 }
 
 export function HabitDay() {
     return (
-        <div className="w-10 h-10 bg-zinc-900 border-zinc-800 border-2 rounded-lg"></div>
+        <Popover.Root>
+            <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-zinc-800 border-2 rounded-lg" />
+
+            <Popover.Portal>
+                <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
+                    <span className='font-semibold text-zinc-400'>Segunda-feira</span>
+                    <span className='mt-1 font-extrabold leading-tight text-3xl'>19/01</span>
+                    <Popover.Arrow className='fill-zinc-900'/>
+
+                   <ProgressBar progress={40}/>
+
+                </Popover.Content>
+            </Popover.Portal>
+        </Popover.Root>
+
     )
 }
